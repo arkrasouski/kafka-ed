8172719a3cba28be7b3ecdc057ce47aa193a68a1
root <root@Analitik166.patio-minsk.by>
2025-10-01 21:17:58 +0300

    feat: clean folders

--- initdb/01-init.sql
+++
@@ -1,5 +0,0 @@
-ALTER SYSTEM SET wal_level = 'logical';
-ALTER SYSTEM SET max_wal_senders = 5;
-ALTER SYSTEM SET max_replication_slots = 5;
-
-ALTER ROLE debezium WITH REPLICATION LOGIN PASSWORD 'dbz';
